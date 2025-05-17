9547e7b39c86bbe108a11a44eb3bcb8e8c07c4b4
Ishu <ishukashyap290@gmail.com>
2025-05-17 22:14:56 +0530

    add js

---
+++ Js.js
@@ -0,0 +1,18 @@
+value = 5 ; //this caled assigning value to variable
+console.log(value); // this used for print in console
+console.log(value * 5);
+
+ let  a= 6;
+ let  b= a +11;
+ console.log(b);
+
+  a= 10 // re-assigning value to variable 
+  // while re-assigning no need to add let cause we can't add two value to same variable but we can update without ussing let
+ console.log(a)
+
+ let convo = "a great china wall" ;let song = 3
+ console.log(convo);
+ convo = "statue of liberty"
+ console.log(convo)
+
+ // amazon cart project 
