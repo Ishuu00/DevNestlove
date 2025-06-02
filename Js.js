045f53f5ca2b0555b2195cdb096eaa6a1d1e66ba
Ishu <ishukashyap290@gmail.com>
2025-06-02 16:46:56 +0530

    learned data type objects

--- Js.js
+++ Js.js
@@ -2,17 +2,61 @@ value = 5 ; //this caled assigning value to variable
 console.log(value); // this used for print in console
 console.log(value * 5);
 
- let  a= 6;
- let  b= a +11;
- console.log(b);
+let  a= 6;
+let  b= a +11;
+console.log(b);
 
   a= 10 // re-assigning value to variable 
   // while re-assigning no need to add let cause we can't add two value to same variable but we can update without ussing let
- console.log(a)
+console.log(a)
 
- let convo = "a great china wall" ;let song = 3
- console.log(convo);
- convo = "statue of liberty"
- console.log(convo)
+let convo = "a great china wall" ;let song = 3
+console.log(convo);
+convo = "statue of liberty"
+console.log(convo)
 
  // amazon cart project 
+let y ; //undefined 
+const z =56; // number 
+isadd = true // boolean
+isnotadd = false // boolean
+let vika ; // undefined
+let ishu = null // object somehting should be there but not sure what
+
+let employee = {
+    name : "ishhuu",
+    age : 19,
+    tenurityPeriod : 1 ,
+    process : "myntra",
+    isSelected : true,
+    company : "Ey"
+};
+console.log(employee["company"]);
+console.log(employee.name)
+ //object is collection of data
+const friend = {
+   circleSize : 5,
+   city : "delhi",
+   isintouch : true,
+   nottouch : 1,
+}
+
+const product = {
+   title : "ballpen",
+   rating : 4,
+   isdeal : true,
+   price : 250,
+}
+const profile ={
+   name : "ishu",
+   post : 195,
+   isfollow : false,
+   followers : 284,
+   following : 44,
+   discription : "Enterprenuer"
+};
+profile.name = "ishu kashyap" // update keys
+profile.discription = "Devloper | student"
+console.log(typeof profile.isfollow); //can ask type 
+
+ 
